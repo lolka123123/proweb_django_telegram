e2e8de6dc2c6ae7ae3d8e881d1a246408890eb43
ovash <khvan525@gmail.com>
2023-12-26 21:12:03 +0500

    0.2

--- project/application/management/commands/callbacks.py
+++ project/application/management/commands/callbacks.py
@@ -42,7 +42,7 @@ def alphabet(call: CallbackQuery):
 
         for word in words:
             if word.translated_word.lower()[0] == data.lower():
-                text += f"{word.translated_word} - {word.word} {word.language} | {LANGUAGES[word.translated_to_language]} - {LANGUAGES[word.translated_language]}\n"
+                text += f"{word.translated_word} - {word.word} | {LANGUAGES[word.translated_to_language]} - {LANGUAGES[word.translated_language]}\n"
 
         if text == '':
             bot.edit_message_text(text='<b>Тут пусто</b>', chat_id=call.message.chat.id, message_id=call.message.message_id,
